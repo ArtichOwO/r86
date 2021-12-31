2ca09e473f0578e751a8c4cca9a597780ee9f64b
ArtichautCosmique <leo.grange@icloud.com>
2021-12-31 17:09:08 +0100

    fix(log): Output errors in stderr instead of stdout

--- lib/log.ml
+++ lib/log.ml
@@ -17,7 +17,7 @@ type msg =
 let log = function
   | Log l -> Printf.printf "%s:%s \n" (yellow Sys.argv.(1)) l
   | SysError s ->
-      Printf.printf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(0)) s
-  | Error e -> Printf.printf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(1)) e
+      Printf.eprintf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(0)) s
+  | Error e -> Printf.eprintf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(1)) e
   | Warning w ->
-      Printf.printf "%s:%s:%s \n" (yellow Sys.argv.(1)) (magenta "warn") w
+      Printf.eprintf "%s:%s:%s \n" (yellow Sys.argv.(1)) (magenta "warn") w
