a6e00f004c9c1758431a6d8c44aabfd421a60c17
ArtichautCosmique <leo.grange@icloud.com>
2021-12-31 17:12:07 +0100

    fix(log): clean

--- lib/log.ml
+++ lib/log.ml
@@ -18,6 +18,7 @@ let log = function
   | Log l -> Printf.printf "%s:%s \n" (yellow Sys.argv.(1)) l
   | SysError s ->
       Printf.eprintf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(0)) s
-  | Error e -> Printf.eprintf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(1)) e
+  | Error e ->
+      Printf.eprintf "%s:%s:%s \n" (red "error") (yellow Sys.argv.(1)) e
   | Warning w ->
       Printf.eprintf "%s:%s:%s \n" (yellow Sys.argv.(1)) (magenta "warn") w
